2a74b8a457def82765d9f9e3583a2016c7b29cd3
mauruch <capolupo.mauro@gmail.com>
2015-03-28 15:43:45 -0300

    Classes to parse and build Stage

---
+++ headers/Stage.h
@@ -0,0 +1,17 @@
+/*
+ * Stage.h
+ *
+ *  Created on: 28/3/2015
+ *      Author: mauro
+ */
+
+#ifndef STAGE_H_
+#define STAGE_H_
+
+class Stage {
+public:
+	Stage();
+	virtual ~Stage();
+};
+
+#endif /* STAGE_H_ */
---
+++ src/Stage.cpp
@@ -0,0 +1,18 @@
+/*
+ * Stage.cpp
+ *
+ *  Created on: 28/3/2015
+ *      Author: mauro
+ */
+
+#include "../headers/Stage.h"
+
+Stage::Stage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Stage::~Stage() {
+	// TODO Auto-generated destructor stub
+}
+
