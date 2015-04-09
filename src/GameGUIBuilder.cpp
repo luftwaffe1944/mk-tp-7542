e7a30f26d302ef1d837641714af8138e06b65935
diego.serra <diego.serra@gmail.com>
2015-04-09 18:18:30 -0300

    validacion de ventana, si alto o ancho fuera de rango se define por defecto. Si ancho logico menor a cero, se define por defecto. Se ubica la ventana en el centro de la pantalla.

--- src/GameGUIBuilder.cpp
+++ src/GameGUIBuilder.cpp
@@ -17,6 +17,119 @@ GameGUIBuilder::~GameGUIBuilder() {
 	// TODO Auto-generated destructor stub
 }
 
+float jsonGetRatio(Json::Value root) {
+	Json::Value windowValue = root[JSON_KEY_VENTANA];
+	int win_width_px = windowValue.get(JSON_KEY_ANCHOPX, 700).asInt();
+	int win_width = windowValue.get(JSON_KEY_ANCHO, 700).asInt();
+	float ratio = win_width_px / win_width;
+	FILE_LOG(logDEBUG) << "Windows Ratio (widht_px / width_lg): " << ratio;
+	return ratio;
+
+}
+
+Window jsonGetWindow(Json::Value root) {
+
+	Json::Value windowValue = root[JSON_KEY_VENTANA];
+	int win_width_px = windowValue.get(JSON_KEY_ANCHOPX, 700).asInt();
+	int win_height_px = windowValue.get(JSON_KEY_ALTOPX, 700).asInt();
+	int win_width = windowValue.get(JSON_KEY_ANCHO, 700).asInt();
+	FILE_LOG(logDEBUG) << "JSON - Window width: " << win_width_px << "px";
+	FILE_LOG(logDEBUG) << "JSON - Window height: " << win_height_px << "px";
+	FILE_LOG(logDEBUG) << "JSON - Window width: " << win_width;
+	if (!((win_width_px>0) && (win_width_px <=MAX_WINDOW_WIDTH_PX) && (win_height_px>0) && (win_height_px <=MAX_WINDOW_HEIGHT_PX) && (win_width>0))){
+			if (!(win_width>0)){
+				FILE_LOG(logWARNING) << "Window logical width out of range. Set by default: " <<DEFAULT_WINDOW_WIDTH;
+				win_width = DEFAULT_WINDOW_WIDTH;
+			}
+			else
+			{
+				if (!((win_width_px>0) && (win_width_px <=MAX_WINDOW_WIDTH_PX))){
+					FILE_LOG(logWARNING) << "Window width px out of range (" <<"0 - "<< MAX_WINDOW_WIDTH_PX<<")";
+				}else if (!((win_height_px>0) && (win_height_px <=MAX_WINDOW_HEIGHT_PX))){
+					FILE_LOG(logWARNING) << "Window height px out of range (" <<"0 - "<< MAX_WINDOW_HEIGHT_PX<<")";
+				}
+				FILE_LOG(logWARNING) << "Window set to default values (" << DEFAULT_WINDOW_WIDTH_PX<<"-"<<DEFAULT_WINDOW_HEIGHT_PX<<")";
+				win_width_px=DEFAULT_WINDOW_WIDTH_PX;
+				win_height_px=DEFAULT_WINDOW_HEIGHT_PX;
+			}
+	}
+	int windowXpos = ((MAX_WINDOW_WIDTH_PX-win_width_px)/2);
+	int windowYpos = ((MAX_WINDOW_HEIGHT_PX-win_height_px)/2);
+
+	Window window(GAME_TITLE, windowXpos, windowYpos, win_width_px, win_height_px, win_width);
+	return window;
+}
+
+Stage jsonGetStage(Json::Value root) {
+	Json::Value stageValue = root[JSON_KEY_ESCENARIO];
+	int stage_width = stageValue.get(JSON_KEY_ANCHO, 700).asInt();
+	int stage_win_height = stageValue.get(JSON_KEY_ALTO, 700).asInt();
+	int stage_win_ypiso = stageValue.get(JSON_KEY_YPISO, 700).asInt();
+	FILE_LOG(logDEBUG) << "JSON - Stage width: " << stage_width;
+	FILE_LOG(logDEBUG) << "JSON - Stage height: " << stage_win_height;
+	FILE_LOG(logDEBUG) << "JSON - Stage ypiso: " << stage_win_ypiso;
+	Stage stage(stage_width, stage_win_height, stage_win_ypiso);
+	return stage;
+}
+
+vector<Layer*> jsonGetLayers(Json::Value root, float ratio) {
+
+	Json::Value windowValue = root[JSON_KEY_VENTANA];
+	int win_height_px = windowValue.get(JSON_KEY_ALTOPX, 700).asInt();
+
+	const Json::Value array = root[JSON_KEY_CAPAS];
+	vector<Layer*> layers;
+	string path;
+	int width;
+	for (unsigned int index = 0; index < array.size(); ++index) {
+		path = array[index].get(JSON_KEY_IMAGEN_FONDO, "png").asString();
+		width = array[index].get(JSON_KEY_ANCHO, 50).asInt();
+
+		stringstream sLayerName;
+		sLayerName.clear();
+		sLayerName << "layer";
+		sLayerName << index;
+
+		//TODO calcular ratio
+		Layer* layer = new Layer(new LoaderParams(0, 0, width, win_height_px, index, ratio,	sLayerName.str()));
+
+		//Add layers to the game loop
+		MKGame::Instance()->getObjectList().push_back(layer);
+
+		FILE_LOG(logDEBUG) << "JSON - Layer" << index << " background image: "	<< path;
+		FILE_LOG(logDEBUG) << "JSON - Layer" << index << " width: " << width;
+		layers.push_back(layer);
+		return layers;
+	}
+
+}
+
+vector<Character*> jsonGetCharacters(Json::Value root, float ratio) {
+	Json::Value characterValue = root[JSON_KEY_PERSONAJE];
+	string character_name =	characterValue.get(JSON_KEY_NOMBRE, "nombre").asString();
+	int character_width = characterValue.get(JSON_KEY_ANCHO, 700).asInt();
+	int character_height = characterValue.get(JSON_KEY_ALTO, 700).asInt();
+	int character_zindex = characterValue.get(JSON_KEY_ZINDEX, 700).asInt();
+	string character_orientation = characterValue.get(JSON_KEY_ORIENTACION,	"right").asString();
+
+	FILE_LOG(logDEBUG) << "JSON - Character name: " << character_name;
+	FILE_LOG(logDEBUG) << "JSON - Character width: " << character_width;
+	FILE_LOG(logDEBUG) << "JSON - Character height: " << character_height;
+	FILE_LOG(logDEBUG) << "JSON - Character z-index: " << character_zindex;
+	FILE_LOG(logDEBUG) << "JSON - Character orientation: " << character_orientation;
+
+	vector<Character*> characters;
+	Character* playerOne = new Character(new LoaderParams(0, 0, character_width, character_height, character_zindex, ratio,	character_name));
+
+	//Add player to the game loop
+	playerOne->setImagePath("images/scorpion_fighting_stance/sfsGIF.gif");
+	MKGame::Instance()->getObjectList().push_back(playerOne);
+	cout << MKGame::Instance()->getObjectList().size() << endl;
+
+	characters.push_back(playerOne);
+	return characters;
+}
+
 //TODO Fixme
 GameGUI* GameGUIBuilder::create() {
 
@@ -29,96 +142,29 @@ GameGUI* GameGUIBuilder::create() {
 	ifstream gameConfig("src/stageConfig.json", std::ifstream::binary);
 
 	if (!gameConfig.good()) {
-		MessageError fileNotFound(ERROR_FILE_NOT_FOUND, FILE_CONFIG_NOT_FOUND,
-				LOG_LEVEL_ERROR);
+		MessageError fileNotFound(ERROR_FILE_NOT_FOUND, FILE_CONFIG_NOT_FOUND, LOG_LEVEL_ERROR);
 		Log<Output2FILE>::logMsgError(fileNotFound);
 		return createDefault();
 	}
 
 	bool parsingSuccessful = reader.parse(gameConfig, root, false);
 	if (!parsingSuccessful) {
-		MessageError parseException(ERROR_PARSER_JSON,
-				reader.getFormattedErrorMessages(), LOG_LEVEL_ERROR);
+		MessageError parseException(ERROR_PARSER_JSON, reader.getFormattedErrorMessages(), LOG_LEVEL_ERROR);
 		Log<Output2FILE>::logMsgError(parseException);
+		return createDefault();
 	}
 
-	Json::Value windowValue = root[JSON_KEY_VENTANA];
-	int win_width_px = windowValue.get(JSON_KEY_ANCHOPX, 700).asInt();
-	int win_height_px = windowValue.get(JSON_KEY_ALTOPX, 700).asInt();
-	int win_width = windowValue.get(JSON_KEY_ANCHO, 700).asInt();
 
-	float ratio = win_width_px / win_width;
-	FILE_LOG(logDEBUG) << "Windows Ratio: " << ratio;
 
-	FILE_LOG(logDEBUG) << "JSON - Windows width: " << win_width_px << "px";
-	FILE_LOG(logDEBUG) << "JSON - Windows height: " << win_height_px << "px";
-	FILE_LOG(logDEBUG) << "JSON - Windows width: " << win_width;
-	Window window(GAME_TITLE, 100, 100, win_width_px, win_height_px, win_width);
+	Window window = jsonGetWindow(root);
 
-	Json::Value stageValue = root[JSON_KEY_ESCENARIO];
-	int stage_width = stageValue.get(JSON_KEY_ANCHO, 700).asInt();
-	int stage_win_height = stageValue.get(JSON_KEY_ALTO, 700).asInt();
-	int stage_win_ypiso = stageValue.get(JSON_KEY_YPISO, 700).asInt();
-	FILE_LOG(logDEBUG) << "JSON - Stage width: " << stage_width;
-	FILE_LOG(logDEBUG) << "JSON - Stage height: " << stage_win_height;
-	FILE_LOG(logDEBUG) << "JSON - Stage ypiso: " << stage_win_ypiso;
-	Stage stage(stage_width, stage_win_height, stage_win_ypiso);
+	float ratio = jsonGetRatio(root);
 
-	const Json::Value array = root[JSON_KEY_CAPAS];
-	vector<Layer*> layers;
-	string backgroundImage;
-	int layerWidth;
-	for (unsigned int index = 0; index < array.size(); ++index) {
-		backgroundImage =
-				array[index].get(JSON_KEY_IMAGEN_FONDO, "png").asString();
-		layerWidth = array[index].get(JSON_KEY_ANCHO, 50).asInt();
+	Stage stage = jsonGetStage(root);
 
-		stringstream sLayerName;
-		sLayerName.clear();
-		sLayerName << "layer";
-		sLayerName << index;
+	vector<Layer*> layers = jsonGetLayers(root, ratio);
 
-		Layer* layer = new Layer(
-				new LoaderParams(0, 0, 128, 82, index, ratio,
-						sLayerName.str()));
-
-		//Add layers to the game loop
-		MKGame::Instance()->getObjectList().push_back(layer);
-
-		FILE_LOG(logDEBUG) << "JSON - Layer" << index << " background image: "
-				<< backgroundImage;
-		FILE_LOG(logDEBUG) << "JSON - Layer" << index << " width: "
-				<< layerWidth;
-		layers.push_back(layer);
-	}
-
-	Json::Value characterValue = root[JSON_KEY_PERSONAJE];
-	string character_name =
-			characterValue.get(JSON_KEY_NOMBRE, "nombre").asString();
-	int character_width = characterValue.get(JSON_KEY_ANCHO, 700).asInt();
-	int character_height = characterValue.get(JSON_KEY_ALTO, 700).asInt();
-	int character_zindex = characterValue.get(JSON_KEY_ZINDEX, 700).asInt();
-	string character_orientation = characterValue.get(JSON_KEY_ORIENTACION,
-			"right").asString();
-
-	FILE_LOG(logDEBUG) << "JSON - Character name: " << character_name;
-	FILE_LOG(logDEBUG) << "JSON - Character width: " << character_width;
-	FILE_LOG(logDEBUG) << "JSON - Character height: " << character_height;
-	FILE_LOG(logDEBUG) << "JSON - Character z-index: " << character_zindex;
-	FILE_LOG(logDEBUG) << "JSON - Character orientation: "
-			<< character_orientation;
-
-	vector<Character*> characters;
-	Character* playerOne = new Character(
-			new LoaderParams(0, 0, 128, 82, character_zindex, ratio,
-					"scorpion"));
-
-	//Add player to the game loop
-	playerOne->setImagePath("images/scorpion_fighting_stance/sfsGIF.gif");
-	MKGame::Instance()->getObjectList().push_back(playerOne);
-	cout << MKGame::Instance()->getObjectList().size() << endl;
-
-	characters.push_back(playerOne);
+	vector<Character*> characters = jsonGetCharacters(root, ratio);
 
 	gameGUI->setWindow(window);
 	gameGUI->setStage(stage);
