e5ad0eaaef1dfa5eb9191b1cf2c7bfc4bec543d4
mauruch <capolupo.mauro@gmail.com>
2015-05-12 18:12:02 -0300

    sprites

---
+++ headers/CharacterManager.h
@@ -0,0 +1,31 @@
+/*
+ * CharacterManager.h
+ *
+ *  Created on: 12/5/2015
+ *      Author: mauro
+ */
+
+#ifndef CHARACTERMANAGER_H_
+#define CHARACTERMANAGER_H_
+
+#include "GameGUI.h"
+
+using namespace std;
+
+class CharacterManager {
+public:
+	static CharacterManager* Instance();
+	virtual ~CharacterManager() {
+		delete(cm_pInstance);
+		cm_pInstance = 0;
+	}
+
+	void solveMovesBeignHint();
+	void solveMovesBeignHint(DamageObject* actualObj, DamageObject* nextObj);
+
+private:
+	CharacterManager();
+	static CharacterManager* cm_pInstance;
+};
+
+#endif /* CHARACTERMANAGER_H_ */
