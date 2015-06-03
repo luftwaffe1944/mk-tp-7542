6321bd02bb671311a471cfe4beb324355b7d0045
uje <jmujeda@gmail.com>
2015-06-03 00:20:19 -0300

    Creada la clase SoundManager con los métodos que marca el libro de SDL2 game programming

---
+++ headers/SoundManager.h
@@ -0,0 +1,40 @@
+/*
+ * SoundManager.h
+ *
+ *  Created on: 2/6/2015
+ *      Author: uje
+ */
+
+
+#ifndef SOUNDMANAGER_H_
+#define SOUNDMANAGER_H_
+
+#include "Constants.h"
+#include "SDL_mixer.h"
+#include <map>
+
+enum sound_type
+{
+	SOUND_MUSIC = 0,
+	SOUND_SFX = 1
+};
+
+class SoundManager {
+public:
+	static SoundManager* Instance();
+	virtual ~SoundManager() {
+		delete(cm_pInstance);
+		cm_pInstance = 0;
+	}
+	bool load(std::string fileName, std::string id, sound_type type);
+	void playSound(std::string id, int loop);
+	void playMusic(std::string id, int loop);
+
+private:
+	SoundManager();
+	static SoundManager* cm_pInstance;
+	std::map<std::string, Mix_Chunk*> m_sfxs;
+	std::map<std::string, Mix_Music*> m_music;
+};
+
+#endif /* SOUNDMANAGER_H_ */
