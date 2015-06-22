596e1932c59bae361aef78327e744c8d1b4e7427
Julian Reale <julianreale@gmail.com>
2015-06-21 23:15:46 -0300

    fix letra mayuscula

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -417,8 +417,8 @@ void Menu::buttonJoystickZero() {
 
 void Menu::showTextBox() {
 	SDL_Color fColor = { 255, 255, 255, 255 };
-	TTF_Font* font = TTF_OpenFont("fonts/mk3.ttf", 40);
-	//TTF_SetFontStyle(font, TTF_STYLE_ITALIC);
+	TTF_Font* font = TTF_OpenFont("fonts/MK3.ttf", 40);
+	TTF_SetFontStyle(font, TTF_STYLE_ITALIC);
 	float ratioX = TextureManager::Instance()->ratioWidth;
 	float ratioY = TextureManager::Instance()->ratioHeight;
 	int windowsWidth = GameGUI::getInstance()->getWindow()->getWidthPx() / ratioX;
