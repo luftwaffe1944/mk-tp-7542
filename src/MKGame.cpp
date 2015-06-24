2f590d5ae62fa0dc9c4f5e9fb42cf8722e59a334
Nicolas <nicolas.m.outeda@accenture.com>
2015-06-23 21:58:03 -0300

    clear mk game flags que usa gameinfo

--- src/MKGame.cpp
+++ src/MKGame.cpp
@@ -77,6 +77,11 @@ bool MKGame::init(GameGUI* gameGui) {
 
 	FILE_LOG(logDEBUG) << "init success";
 	m_bRunning = true;
+	showFatality = false;
+	showBabality = false;
+	showFriendship = false;
+	isFinishimMoment = false;
+	lazyAnimationAlreadyTriggered = false;
 	return true;
 }
 
